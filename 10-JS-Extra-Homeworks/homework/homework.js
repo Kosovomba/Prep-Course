71adb6ef43fdb0a4bd49b292d3f5348eadd58523
Kosovomba <santiagojavierlevy@gmail.com>
2022-03-18 05:50:45 -0300

    JSX test incomplete

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var objetoPropiedades = Object.keys(objeto)
+  var objetoMatriz = []
+  for(i = 0; i < objetoPropiedades.length; i++) {
+    objetoMatriz[i] = [objetoPropiedades[i], objeto[objetoPropiedades[i]]]
+  }
+  return objetoMatriz
 }
 
 
@@ -18,6 +24,26 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {}
+  for (i = 0; i < string.length; i++) {
+    var objetoPropiedades = Object.keys(objeto)
+    var a = 1
+    var n = string[i]
+    var h = 0
+    for (m = 0; m < objetoPropiedades.length; m++){
+      if(n == objetoPropiedades[m])
+      h = h + 1
+    }
+    if (h == 0) {
+      for (j = i + 1; j < string.length; j++) {
+        if (n == string[j]) {
+          a = a + 1
+        }
+      }
+      objeto[n] = a
+    }
+  }
+  return objeto
 }
 
 
@@ -43,6 +69,17 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numero2 = numero.toString();
+  let a = 0;
+  for (i = 0; i < Math.trunc(numero2.length/2); i++) {
+    if (numero2[i] == numero2[numero2.length - i - 1]) {
+      a = a + 1
+    }
+  }
+  if (Math.trunc(numero2.length/2) == a) {
+    return "Es capicua"
+  }
+  return "No es capicua"
 }
 
 
@@ -50,6 +87,13 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  for(i = 0; i < cadena.length; i++) {
+    if(cadena[i] == 'a'|| cadena[i] == 'b' || cadena[i] == 'c'){
+      cadena = cadena.replace(cadena[i], '')
+      i = i - 1
+    }
+  }
+  return cadena
 }
 
 
@@ -66,6 +110,19 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var arreglo3 = []
+  for(i = 0; i < arreglo1.length; i++) {
+    var a = 0
+    for(j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] == arreglo2[j]){
+        a = a + 1
+      }
+    }
+    if(a > 0) {
+      arreglo3.push(arreglo1[i])
+    }
+  }
+  return arreglo3
 }
 
 
